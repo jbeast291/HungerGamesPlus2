848d2f5f39ed85b765247ad1bf89be15dc13c471
jbeast291 <jacoby29181@gmail.com>
2024-02-04 12:52:47 -0800

    first commit

---
+++ src/main/java/hgplus/hungergamesplus2/HungerGamesPlus2.java
@@ -0,0 +1,17 @@
+package hgplus.hungergamesplus2;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class HungerGamesPlus2 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
